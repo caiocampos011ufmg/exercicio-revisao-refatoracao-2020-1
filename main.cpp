034914c163e4479a5c2dca3a4bc9e682270a50e3
CAIO AUGUSTO CAMPOS <72575045+caiocampos011ufmg@users.noreply.github.com>
2020-10-21 14:23:18 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,64 +1,73 @@
 #include "Engenheiro.cpp"
 #include "Vendedor.cpp"
+#define TAM 3
 
 int main() {
+    
+  int i;
+  int j;
+//eng e vend sao vetores que recebem dados dos vendedores e engenheiros
+  Vendedor *vend=new Vendedor[TAM];
+  Engenheiro *eng=new Engenheiro[TAM] ;
+  //enserindo dados do engenheiro 1
+  eng[0].nome = "Joao Snow";
+  eng[0].salarioHora = 35;
+  eng[0].projetos = 3; 
+  eng[0].horas_trabalho=9.5;
+   
+  
+  //inserindo dados do engenheiro 2
+  
+  eng[1].nome = "Daniela Targaryen";
+  eng[1].salarioHora = 30;
+  eng[1].projetos = 1; 
+  eng[1].horas_trabalho=8;
+ 
+ 
+    //inserindo dados do engenheiro 3
 
-  Engenheiro eng1;
-  eng1.nome = "Joao Snow";
-  eng1.salarioHora = 35;
-  eng1.projetos = 3; 
-  std::cout << "Nome: " << eng1.nome << std::endl;
-  std::cout << "Salario Mes: " << eng1.pagamentoMes(9.5) << std::endl;
-  std::cout << "Projetos: " << eng1.projetos << std::endl;
-  std::cout << std::endl;
+  eng[2].nome = "Bruno Stark";
+  eng[2].salarioHora = 30;
+  eng[2].projetos = 2; 
+  eng[2].horas_trabalho=8;
+   //imprimindo dados engenheios  
+   for( i=0;i<TAM;i++){
+        std::cout << "Nome: " << eng[i].nome << std::endl;
+        std::cout << "Salario Mes: " << eng[i].pagamentoMes (eng[i].horas_trabalho) << std::endl;
+        std::cout << "Projetos: " << eng[i].projetos << std::endl;
+        std::cout << std::endl;
+      
+    }
+   
+   
+    //inserindo dados do vendedor 1
   
-  Engenheiro eng2;
-  eng2.nome = "Daniela Targaryen";
-  eng2.salarioHora = 30;
-  eng2.projetos = 1; 
-  std::cout << "Nome: " << eng2.nome << std::endl;
-  std::cout << "Salario Mes: " << eng2.pagamentoMes(8) << std::endl;
-  std::cout << "Projetos: " << eng2.projetos << std::endl;  
-  std::cout << std::endl;
+  vend[0].nome = "Tonho Lannister";
+  vend[0].salarioHora = 20;
+  vend[0].quotaMensalVendas = 5000;
+  vend[0].horas_trabalho=6;
   
-  Engenheiro eng3;
-  eng3.nome = "Bruno Stark";
-  eng3.salarioHora = 30;
-  eng3.projetos = 2; 
-  std::cout << "Nome: " << eng3.nome << std::endl;
-  std::cout << "Salario Mes: " << eng3.pagamentoMes(8) << std::endl;
-  std::cout << "Projetos: " << eng3.projetos << std::endl;  
-  std::cout << std::endl;
+    //inserindo dados do vendedor 2
+
+  vend[1].nome = "Jose Mormont";
+  vend[1].salarioHora = 25;
+  vend[1].quotaMensalVendas = 3000;
+   vend[1].horas_trabalho=8;
   
-  
-  Vendedor vend1;
-  vend1.nome = "Tonho Lannister";
-  vend1.salarioHora = 20;
-  vend1.quotaMensalVendas = 5000;
-  
-  std::cout << "Nome: " << vend1.nome << std::endl;
-  std::cout << "Salario Mes: " << vend1.pagamentoMes(6) << std::endl;  
-  std::cout << "Quota vendas: " << vend1.quotaTotalAnual() << std::endl;
-  std::cout << std::endl;
-  
-  Vendedor vend2;
-  vend2.nome = "Jose Mormont";
-  vend2.salarioHora = 25;
-  vend2.quotaMensalVendas = 3000;
-  
-  std::cout << "Nome: " << vend2.nome << std::endl;
-  std::cout << "Salario Mes: " << vend2.pagamentoMes(8) << std::endl;  
-  std::cout << "Quota vendas: " << vend2.quotaTotalAnual() << std::endl; 
-  std::cout << std::endl;  
-	
-  Vendedor vend3;
-  vend3.nome = "Sonia Stark";
-  vend3.salarioHora = 30;
-  vend3.quotaMensalVendas = 4000;
-  
-  std::cout << "Nome: " << vend3.nome << std::endl;
-  std::cout << "Salario Mes: " << vend3.pagamentoMes(8) << std::endl;  
-  std::cout << "Quota vendas: " << vend3.quotaTotalAnual() << std::endl;  
-  
+	  //inserindo dados do vendedor 3
+  vend[2].nome = "Sonia Stark";
+  vend[2].salarioHora = 30;
+  vend[2].quotaMensalVendas = 4000;
+  vend[2].horas_trabalho=8;
+  //imprimindo dados do vendedores
+for( j=0;j<TAM;j++) {
+     std::cout << "Nome: " << vend[j].nome << std::endl;
+        std::cout << "Salario Mes: " << vend[j].pagamentoMes (vend[j].horas_trabalho) << std::endl;
+        std::cout << "Quota vendas: " << vend[j].quotaTotalAnual () << std::endl;
+        std::cout << std::endl;
+        
+    }
+  delete [] eng;
+  delete [] vend;
   return 0;	
 }
